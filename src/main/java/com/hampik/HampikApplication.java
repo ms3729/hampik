48339676ba7d68501171e3573395229a6728cedc
Ehsan Shishehbor <ms3729@gmail.com>
2026-08-20 13:56:43 +0330

    commit base files

---
+++ src/main/java/com/hampik/HampikApplication.java
@@ -0,0 +1,13 @@
+package com.hampik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HampikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HampikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hampik/HampikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hampik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HampikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
